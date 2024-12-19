23f9a2c4fbd34122853534a01030d79d82ba5986
XavierElCapitan <xavierelcapitan@gmail.com>
2024-12-19 13:06:53 +0100

    suppresion vardump home

--- Views/home.php
+++ Views/home.php
@@ -38,13 +38,7 @@
     <section class="experience-section text-center mt-5">
         <h3 class="experience-title mb-2 px-4">Vivez une expérience inoubliable au Zoo d'Arcadia</h3>
         <p class="experience-subtitle m-4">Quelle expérience préférez-vous ?</p>
-        <?php
-var_dump($todaySchedule);
-?>
     </section>
-    
-
-  
 
     <!-- Section card expériences -->
 <section class="experience-card">
